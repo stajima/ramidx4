019fef710253e99e0f893b4066d3aa8923602b89
Shane <shane@meyercomputer.com>
2016-10-18 13:01:15 -1000

    changes

--- static/appSetup.js
+++ static/appSetup.js
@@ -1,20 +1,41 @@
 console.log('start of app setup');
-window.appBootstrap = window.appBootstrap || function (scriptUrl) {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = false;
-        script.defer = false;
-        script.src = scriptUrl;
-        head.appendChild(script);
-    };
-appBootstrap('https://npmcdn.com/zone.js@0.6.12');
-appBootstrap('https://npmcdn.com/reflect-metadata@0.1.3');
-appBootstrap('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js');
-appBootstrap('https://npmcdn.com/typescript@1.8.10/lib/typescript.js');
-appBootstrap('https://pickettd.github.io/server-example-shane/config.js');
+function loadScript(url, callback) {
 
-(function () {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState == "loaded" ||
+                script.readyState == "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+loadScript('https://npmcdn.com/zone.js@0.6.12', function () {
+    console.log('zone loaded');
+});
+loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
+    console.log('reflect loaded');
+});
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
+    console.log('systemjs loaded');
+});
+loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
+    console.log('typescript loaded');
+});
+loadScript('https://pickettd.github.io/server-example-shane/config.js', function () {
+    console.log('example loaded');
     System.import('app').catch(console.error.bind(console));
-})();
+});
+
 console.log('end of app setup');
