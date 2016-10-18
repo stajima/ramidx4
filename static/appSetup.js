b34810ff9b4903de88af14b445df1a970ab468c4
Shane <shane@meyercomputer.com>
2016-10-18 13:17:37 -1000

    changes

--- static/appSetup.js
+++ static/appSetup.js
@@ -21,21 +21,20 @@ function loadScript(url, callback) {
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
 }
+
 loadScript('https://npmcdn.com/zone.js@0.6.12', function () {
     console.log('zone loaded');
 });
 loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
     console.log('reflect loaded');
 });
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
-    console.log('systemjs loaded');
-});
 loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
     console.log('typescript loaded');
 });
 loadScript('https://pickettd.github.io/server-example-shane/config.js', function () {
     console.log('example loaded');
+});
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
+    console.log('systemjs loaded');
     System.import('app').catch(console.error.bind(console));
 });
-
-console.log('end of app setup');
