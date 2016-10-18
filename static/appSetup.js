ae6d488fca15a22b4f579e9d86c3f69de752b963
Shane <shane@meyercomputer.com>
2016-10-18 13:52:15 -1000

    changes

--- static/appSetup.js
+++ static/appSetup.js
@@ -22,19 +22,41 @@ function loadScript(url, callback) {
     document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-loadScript('https://npmcdn.com/zone.js@0.6.12', function () {
-    console.log('zone loaded');
-});
-loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
-    console.log('reflect loaded');
-});
-loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
-    console.log('typescript loaded');
-});
-loadScript('https://rawgit.com/stajima/ramidx4/master/static/config.js', function () {
-    console.log('config loaded');
-});
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
-    console.log('systemjs loaded');
-    System.import('app').catch(console.error.bind(console));
-});
+(function init() {
+    loadScript('https://npmcdn.com/zone.js@0.6.12', function () {
+        console.log('zone loaded');
+        loadZone();
+    });
+})();
+
+var loadZone = function () {
+    loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
+        console.log('reflect loaded');
+        loadSystemJS();
+    })
+};
+
+var loadSystemJS = function () {
+    loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
+        console.log('systemjs loaded');
+        loadTS();
+    })
+};
+
+var loadTS = function () {
+    loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
+        console.log('typescript loaded');
+        loadConfig();
+    })
+};
+
+var loadConfig = function () {
+    loadScript('https://rawgit.com/stajima/ramidx4/master/static/config.js', function () {
+        console.log('config loaded');
+        launch();
+    })
+};
+
+var launch = function () {
+    System.import('app').catch(console.error.bind(console))
+};
