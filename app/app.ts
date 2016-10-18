2299042bc937be2c1543f8b4079e6eac9ea4d22b
Shane <shane@meyercomputer.com>
2016-10-18 13:25:56 -1000

    changes

---
+++ app/app.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: 'my-app',
+    styles: [`
+  h1 {
+    color:#545454;
+    background:#02A8F4;
+    padding:15px;
+    box-shadow:2px 2px 2px 0 rgba(0, 0, 0, 0.3);
+  }
+  `],
+    template: `
+  <div>
+  <h1>Hello from the {{componentName}}.</h1>
+  </div>
+  `
+})
+export class AppComponent {
+    componentName = 'AppComponent';
+}
+bootstrap(AppComponent);
+
