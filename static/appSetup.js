eb4306e346f06814686b4efa6b639334d72b4440
Shane <shane@meyercomputer.com>
2016-10-24 11:51:40 -1000

    all scripts now load at the same time

--- static/appSetup.js
+++ static/appSetup.js
@@ -22,41 +22,39 @@ function loadScript(url, callback) {
     document.getElementsByTagName("head")[0].appendChild(script);
 }
 
+var scriptsLoaded = 0;
+var totalScripts = 5;
 (function init() {
     loadScript('https://npmcdn.com/zone.js@0.6.12', function () {
         console.log('zone loaded');
-        loadZone();
+        scriptsLoaded++;
+        launch();
     });
-}());
-
-var loadZone = function () {
-    loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
-        console.log('reflect loaded');
-        loadSystemJS();
-    })
-};
-
-var loadSystemJS = function () {
-    loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
-        console.log('systemjs loaded');
-        loadTS();
-    })
-};
-
-var loadTS = function () {
-    loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
-        console.log('typescript loaded');
-        loadConfig();
-    })
-};
-
-var loadConfig = function () {
     loadScript('https://rawgit.com/stajima/ramidx4/master/static/config.js', function () {
         console.log('config loaded');
+        scriptsLoaded++;
+        launch();
+    });
+    loadScript('https://npmcdn.com/reflect-metadata@0.1.3', function () {
+        console.log('reflect loaded');
+        scriptsLoaded++;
+        launch();
+    });
+    loadScript('https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.27/system.js', function () {
+        console.log('systemjs loaded');
+        scriptsLoaded++;
+        launch();
+    });
+    loadScript('https://npmcdn.com/typescript@1.8.10/lib/typescript.js', function () {
+        console.log('typescript loaded');
+        scriptsLoaded++;
         launch();
     })
-};
+}());
 
 var launch = function () {
-    System.import('app').catch(console.error.bind(console))
+    if (scriptsLoaded === totalScripts) {
+        console.log('Launching app');
+        //System.import('app').catch(console.error.bind(console));
+    }
 };
